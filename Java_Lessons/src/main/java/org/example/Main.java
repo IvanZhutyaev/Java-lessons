a292938473cb242d4ef72a9421ac0e5c3890c008
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-07 13:30:00 +0300

    Arifmetica

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         String name="Ivan";
         int age=15;
+        final int RETIREMENTAGE=65;
         System.out.println("Hello world");
-        System.out.format("Hello. My name is %s, i am %d years old ", name, age);
+        System.out.format("Hello. My name is %s, i am %d years old, IT'S LESS THAN %d", name, age, RETIREMENTAGE);
     }
 
 
