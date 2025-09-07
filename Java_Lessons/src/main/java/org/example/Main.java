0d21a3110cce84904d3b933401e420c808697739
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-07 13:08:18 +0300

    start

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String name="Ivan";
 
+        System.out.println("Hello world");
+        System.out.format("Hello world! %s", name);
     }
 
 
