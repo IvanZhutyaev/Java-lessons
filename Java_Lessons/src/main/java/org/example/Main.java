c81b35e4bc628468461ad24526ab169e7b78ba46
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-14 14:56:27 +0300

    TASKS

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -4,19 +4,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String numbers="1234567890";
-        StringBuffer sb=new StringBuffer(numbers);
-        System.out.println(sb.substring(3));
-        System.out.println(sb.substring(4,8));
-        System.out.println(sb.replace(3,5,"ABCDE"));
-        sb = new StringBuffer(numbers);
-        System.out.println(sb.reverse());
-        sb.reverse();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите слово: ");
+//        String word = scanner.nextLine();
+//        boolean result = true;
+//        char firstSym = word.charAt(0);
+//        if (!Character.isLetter(firstSym) && firstSym != '_') {
+//            result = false;
+//        }
+//        for (int i = 0; i < word.length(); i++) {
+//            char c = word.charAt(i);
+//            if (!Character.isLetterOrDigit(c) && c != '_') {
+//                result = false;
+//                break;
+//            }
+//        }
+//        System.out.println(result ? "ДА" : "НЕТ");
 
-        sb=new StringBuffer(numbers);
-        System.out.println(sb.delete(5,9));
-        System.out.println(sb.deleteCharAt(1));
-        System.out.println(sb.insert(1, "One"));
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        String word = scanner.nextLine();
+//        String a=word.replace(" ", "");
+//        String reversed = new StringBuilder(a).reverse().toString();
+//        boolean result=(a.equals(reversed));
+//        System.out.println(result);
+
     }
 
 
