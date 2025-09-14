df17512ca22627b793af994ce8eb1c04122b3e98
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-14 13:01:10 +0300

    Methods String

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -4,27 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for(int i=1; i<5; i++){
-            System.out.printf("%d", i);
-        }
-        System.out.println();
-        int j=6;
-        while(j>0){
-            System.out.print(j);
-            j--;
-        }
-        System.out.println();
-        int l=7;
-        do{
-            System.out.print(l);
-            l--;
-        }
-        while (l>0);
-        System.out.println();
-        int[] array = new int[]{1,2,3,4,5};
-        for(int i : array) {
-            System.out.print(i);
-        }
+        
     }
 
 
