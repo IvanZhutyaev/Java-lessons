9fe215b86ce3c830d9a37f7e88a773dcbad7c288
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-21 12:59:59 +0300

    s

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text="Fifa will never regret it";
-        String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
-        for(String word:words){
-            System.out.println(word);
-        }
+        Scanner s = new Scanner(System.in);
+        String n=s.nextLine(); //строка, которую проверяют
+        String a= "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"; //регулярное выражение
+        Pattern pattern =Pattern.compile(a);
+        Matcher matcher = pattern.matcher(n);
+        System.out.println(matcher.matches());
     }
 }
