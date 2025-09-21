6a91e05a6220ace226be7e963db89f3fa754d2db
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-21 14:33:41 +0300

    tasks

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -4,14 +4,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int nums[]=new int[10000];
-        Random rand=new Random();
-        for(int i=0;i<10000;i++){
-            int num=rand.nextInt(10000);
-            nums[i]=num;
-        }
-        for(int num:nums){
-            System.out.println(num);
-        }
+        //        int nums[]=new int[]{1,2,3,4,5};
+//        int sum=0;
+//        for(int i : nums){
+//            sum+=i;
+//        }
+//        System.out.println(sum);
+
+
+//        int nums[]=new int[]{1,2,3,4,5};
+//        int sum=0;
+//        for(int i : nums){
+//            sum+=i;
+//        }
+//        System.out.println(sum/ nums.length);
+
+//        int nums[]=new int[]{1,2,3,4,5};
+//
+//        System.out.println(Arrays.stream(nums).min());
+//        System.out.println(Arrays.stream(nums).max());
+
+//        int nums[]=new int[]{1,2,3,4,2,6,3,2,4,5};
+//        int chislo=2;
+//        int res=0;
+//        for(int i : nums){
+//            if(i==chislo){
+//                res++;
+//            }
+//        }
+//        System.out.println(res);
     }
 }
