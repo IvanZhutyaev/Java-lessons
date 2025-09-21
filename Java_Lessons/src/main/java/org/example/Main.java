41731aa2cb48e632c1c2adee947a21df21c437e9
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-21 12:43:36 +0300

    Regular string

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -4,34 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Введите слово: ");
-//        String word = scanner.nextLine();
-//        boolean result = true;
-//        char firstSym = word.charAt(0);
-//        if (!Character.isLetter(firstSym) && firstSym != '_') {
-//            result = false;
-//        }
-//        for (int i = 0; i < word.length(); i++) {
-//            char c = word.charAt(i);
-//            if (!Character.isLetterOrDigit(c) && c != '_') {
-//                result = false;
-//                break;
-//            }
-//        }
-//        System.out.println(result ? "ДА" : "НЕТ");
-
-
-
-
-//        Scanner scanner = new Scanner(System.in);
-//        String word = scanner.nextLine();
-//        String a=word.replace(" ", "");
-//        String reversed = new StringBuilder(a).reverse().toString();
-//        boolean result=(a.equals(reversed));
-//        System.out.println(result);
-
+        String text="Fifa will never regret it";
+        String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
+        for(String word:words){
+            System.out.println(word);
+        }
     }
-
-
 }
