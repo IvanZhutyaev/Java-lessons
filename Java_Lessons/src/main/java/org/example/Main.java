055fb89a3ee724961746c9f90d5350886eafb655
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-28 13:12:03 +0300

    try...catch...finally

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -4,34 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //        int nums[]=new int[]{1,2,3,4,5};
-//        int sum=0;
-//        for(int i : nums){
-//            sum+=i;
-//        }
-//        System.out.println(sum);
-
-
-//        int nums[]=new int[]{1,2,3,4,5};
-//        int sum=0;
-//        for(int i : nums){
-//            sum+=i;
-//        }
-//        System.out.println(sum/ nums.length);
-
-//        int nums[]=new int[]{1,2,3,4,5};
-//
-//        System.out.println(Arrays.stream(nums).min());
-//        System.out.println(Arrays.stream(nums).max());
-
-//        int nums[]=new int[]{1,2,3,4,2,6,3,2,4,5};
-//        int chislo=2;
-//        int res=0;
-//        for(int i : nums){
-//            if(i==chislo){
-//                res++;
-//            }
-//        }
-//        System.out.println(res);
+        try{
+            int[]numbers=new int[3];
+            numbers[4]=45;
+            System.out.println(numbers[4]);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        System.out.println("Программа завршена");
     }
 }
