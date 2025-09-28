9f406e483799487184e038559e1f4c0aac78931a
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-28 12:44:28 +0300

    Functions

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -3,40 +3,35 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    static int func(int a){
-
-        String b=Integer.toString(a);
-        int result=b.length();
-        return result;
-    }
-    static int[] func1(int[] mas){
-        for (int i=0;i<5;i++){
-            Random random=new Random();
-            int a=random.nextInt();
-            mas[i]=a;
-        }
-        return mas;
-    }
-
-    static String func3(int a){
-        String result=Integer.toBinaryString(a);
-        return result;
-    }
     public static void main(String[] args) {
-        //1
-//        int a=func(999868769);
-//        System.out.println(a);
-
-        //2
-//        int[] mas=new int[5];
-//        int[] result=func1(mas);
-//        for(int i:result){
-//            System.out.println(i);
+        //        int nums[]=new int[]{1,2,3,4,5};
+//        int sum=0;
+//        for(int i : nums){
+//            sum+=i;
 //        }
+//        System.out.println(sum);
 
-        //3
-//        int a=5;
-//        String result=func3(a);
-//        System.out.println(result);
+
+//        int nums[]=new int[]{1,2,3,4,5};
+//        int sum=0;
+//        for(int i : nums){
+//            sum+=i;
+//        }
+//        System.out.println(sum/ nums.length);
+
+//        int nums[]=new int[]{1,2,3,4,5};
+//
+//        System.out.println(Arrays.stream(nums).min());
+//        System.out.println(Arrays.stream(nums).max());
+
+//        int nums[]=new int[]{1,2,3,4,2,6,3,2,4,5};
+//        int chislo=2;
+//        int res=0;
+//        for(int i : nums){
+//            if(i==chislo){
+//                res++;
+//            }
+//        }
+//        System.out.println(res);
     }
 }
