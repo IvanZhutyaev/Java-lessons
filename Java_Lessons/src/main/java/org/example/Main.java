20d7646d767ea8ba633441cc993ded519305a090
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-28 14:43:12 +0300

    enum

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -3,20 +3,13 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+    public enum myEnum{
+        winter,
+        summer,
+        spring,
+        fall
+    }
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            int a = scanner.nextInt();
-            if(a==0){
-                throw new Exception();
-            }
 
-        }
-        catch (InputMismatchException ex){
-            System.out.println("Буквы вводить нельзя!");
-        }
-        catch (Exception ex){
-            System.out.println("0 вводить нельзя!");
-        }
     }
 }
