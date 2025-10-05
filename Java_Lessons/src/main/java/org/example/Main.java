30a01cb6f2501a789285964419b23767a7c02fc9
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-05 12:17:01 +0300

    example for constructor

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -3,11 +3,12 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public enum myEnum{
-        winter,
-        summer,
-        spring,
-        fall
+    enum Country{
+        CANADA("CAD"), POLAND("PLN"), GERMANY("EUR");
+        String currency;
+        Country(String currency){
+            this.currency=currency;
+        }
     }
     public static void main(String[] args) {
 
