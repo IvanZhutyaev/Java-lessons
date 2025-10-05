3452e4801c321947835979c40c7f0c71977953b0
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-05 14:40:46 +0300

    example

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -6,15 +6,19 @@ public class Main {
 
 
     public static void main(String[] args) {
-        FileWriter fw=new FileWriter("sample2.txt");
-        for (int i=1;i<101;i++){
-
-            fw.write(Integer.toString(i)+"\n");
+        FileWriter fw = new FileWriter("file.txt");
+        for (int i = 1;i<101;i++)
+        {
+            fw.write(i + "\n");
         }
-
         fw.close();
-
-        FileReader fr=new FileReader("sample2.txt");
+        FileReader fr=new FileReader("file.txt");
+        Scanner scan=new Scanner(fr);
+        int i =1;
+        while (scan.hasNextLine()){
+            System.out.println(i+":"+scan.nextLine());
+            i++;
+        }
         fr.close();
     }
 }
