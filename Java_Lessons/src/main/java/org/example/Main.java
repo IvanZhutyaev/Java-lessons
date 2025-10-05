6aec24f075cb6769daf2a7627001c9f4b1f3f425
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-05 14:18:06 +0300

    FileWriter and FileReader rules

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -6,59 +6,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-        enum Enum{
-            SUMMER(30),
-            WINTER(-20),
-            FALL(15),
-            SPRING(20);
-            int avgTemp;
-            Enum(int avgTemp){
-                this.avgTemp=avgTemp;
-            }
-        }
-        for (Enum val:Enum.values()){
-            System.out.println(val.name()+" , "+val.avgTemp);
-        }
+        FileWriter fw=new FileWriter("sample2.txt");
+        fw.close();
 
-        enum Role { ADMIN, MODERATOR, USER, GUEST;
-            void getPermissions()
-            {
-
-                switch (this){
-                    case ADMIN -> System.out.println("полный доступ");
-                    case MODERATOR -> System.out.println("управление контентом");
-                    case USER -> System.out.println("чтение и комментриование");
-                    case GUEST -> System.out.println("только чтение");
-
-                }
-
-            }
-
-        }
-        for (Role role:Role.values()){
-            role.getPermissions();
-        }
-
-        enum Operation{
-            PLUS,
-            MINUS,
-            MULTIPLY,
-            DIVIDE;
-
-            void apply(double a, double b){
-                double result=0;
-                switch (this){
-                    case PLUS -> {
-                        result=a+b;
-                    }
-                    case MINUS -> result=a-b;
-                    case MULTIPLY -> result=a*b;
-                    case DIVIDE -> result=a/b;
-                }
-                System.out.println(result);
-            }
-        }
-        Operation plus=Operation.PLUS;
-        plus.apply(5,6);
+        FileReader fr=new FileReader("sample2.txt");
+        fr.close();
     }
 }
