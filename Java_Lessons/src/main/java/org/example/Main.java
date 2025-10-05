746557211e7b636424c6cc23cb579a61fbb3f3e9
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-05 12:37:31 +0300

    example

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -3,19 +3,28 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    enum Country{
-        CANADA("CAD"), POLAND("PLN"), GERMANY("GER");
-        String currency;
-        Country(String currency){
-            this.currency=currency;
-        }
 
-        Country(){}
-
-    }
 
     public static void main(String[] args) {
+        enum Country{
+            CANADA("CAD"), POLAND("PLN"), GERMANY("GER"), PARAGVAI;
+            String currency;
+            Country(String currency){
+                this.currency=currency;
+            }
+
+            Country(){}
+            boolean hasCurrency(){
+                return currency!=null;
+            }
+        }
         for (Country country:Country.values())
-            System.out.println(country+", "+country.currency);
+            if(country.hasCurrency()){
+                System.out.println(country+" has currency, "+country.currency);
+
+            }else {
+                System.out.println(country+" has NOT currency, "+country.currency+" LOL");
+
+            }
     }
 }
