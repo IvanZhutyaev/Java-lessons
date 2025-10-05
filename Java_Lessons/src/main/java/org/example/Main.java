fb59c9e4bcaebb0e91f508145c3634f27af61fff
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-05 12:26:12 +0300

    example for constructor1

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     enum Country{
-        CANADA("CAD"), POLAND("PLN"), GERMANY("EUR");
+        CANADA("CAD"), POLAND("PLN"), GERMANY("GER");
         String currency;
         Country(String currency){
             this.currency=currency;
         }
-    }
-    public static void main(String[] args) {
 
+        Country(){}
+
+    }
+
+    public static void main(String[] args) {
+        for (Country country:Country.values())
+            System.out.println(country+", "+country.currency);
     }
 }
