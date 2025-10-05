3f6676824b14d6c44094b37a934c19bb21010163
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-05 12:47:01 +0300

    example

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -7,7 +7,36 @@ public class Main {
 
     public static void main(String[] args) {
         enum Country{
-            CANADA("CAD"), POLAND("PLN"), GERMANY("GER"), PARAGVAI;
+            CANADA("CAD"){
+                @Override
+                void sayHello(){
+                    System.out.println("Hello");
+                }
+            },
+            POLAND("PLN"){
+                @Override
+                void sayHello(){
+                    System.out.println("nindfowsniofdn");
+                }
+            },
+            GERMANY("EUR"){
+                @Override
+                void sayHello(){
+                    System.out.println("HAI");
+                }
+            },
+            LAOPARAS{
+                @Override
+                void sayHello(){
+                    System.out.println("POshel na mateshy");
+                }
+            },
+            ZIMKABU(){
+                @Override
+                void sayHello(){
+                    System.out.println("Shakalaka");
+                }
+            };
             String currency;
             Country(String currency){
                 this.currency=currency;
@@ -17,6 +46,7 @@ public class Main {
             boolean hasCurrency(){
                 return currency!=null;
             }
+            abstract void sayHello();
         }
         for (Country country:Country.values())
             if(country.hasCurrency()){
