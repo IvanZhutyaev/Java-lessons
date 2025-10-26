122c7cc30992814d234fd65741c0764c00db906b
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-26 12:34:16 +0300

    Interfaces:implements and example

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -1,12 +1,24 @@
 package org.example;
 
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Book b1 = new Book("ABRAM STORY", "ABRAAAAM");
+        b1.print();
+    }
+}
+
 interface Printable{
     void print();
 }
-public class Main {
 
-
-
-
+class Book implements Printable{
+    String name;
+    String author;
+    Book(String name, String author){
+        this.name=name;
+        this.author=author;
+    }
+    public void print(){
+        System.out.println(name+author);
+    }
 }
