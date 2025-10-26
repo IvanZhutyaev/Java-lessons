021c0f2feb3044ce823bb0d03b62d460b47ee461
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-26 12:45:14 +0300

    Interfaces:example

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -10,7 +10,6 @@ public class Main {
 interface Printable{
     void print();
 }
-
 class Book implements Printable{
     String name;
     String author;
@@ -20,5 +19,18 @@ class Book implements Printable{
     }
     public void print(){
         System.out.println(name+author);
+    }
+}
+
+class Journal implements Printable{
+    private String name;
+    String getName(){
+        return name;
+    }
+    Journal(String name){
+        this.name=name;
+    }
+    public void print(){
+        System.out.println(name);
     }
 }
