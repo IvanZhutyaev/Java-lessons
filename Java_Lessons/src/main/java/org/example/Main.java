f4ad8babd7598d2074f6bbfd85ae1abee42a83f9
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-11-23 13:39:02 +0300

    Records

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -1,16 +1,30 @@
 package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        Adress dom_toma = new Adress("Дом Пушкина");
-        Person tom = new Person("Tom", dom_toma);
-        System.out.println(tom.toString());
+//            Adress dom_toma = new Adress("Дом Пушкина");
+//            Person tom = new Person("Tom", dom_toma);
+//            System.out.println(tom.toString());
+        Map<Employee, Integer> salary = new HashMap<>();
+        Employee employee=new Employee("Vasya", 123);
+        Employee employee1=new Employee("Vasya", 123);
+        salary.put(employee1, 1);
+        salary.put(employee,2);
+        System.out.println(salary.get(employee));
+        System.out.println(salary.get(employee1));
+        System.out.println(employee1.equals(employee));
     }
-    record Adress(String adress){}
-    record Person(String name, Adress Adress){
-        @Override
-        public String toString() {
-            return String.format("%s живёт в %s", name, Adress.adress);
-        }
-    }
+//        record Adress(String adress){}
+//        record Person(String name, Adress Adress){
+//            @Override
+//            public String toString() {
+//                return String.format("%s живёт в %s", name, Adress.adress);
+//            }
+//        }
+
+    record Employee(String name, int id){}
 }
 
