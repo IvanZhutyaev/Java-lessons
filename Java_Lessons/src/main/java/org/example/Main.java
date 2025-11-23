18944e03530967ffd00282c1fec6dac8dc1d7a6d
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-11-23 13:26:15 +0300

    Records

--- Java_Lessons/src/main/java/org/example/Main.java
+++ Java_Lessons/src/main/java/org/example/Main.java
@@ -1,55 +1,16 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        Shape circle=new Circle(3.8);
-        Shape rectangle=new Rectangle(2.7, 4.6);
-        Shape triangle=new Triangle(4.5, 3.6);
-        circle.Area();
-        rectangle.Area();
-        triangle.Area();
+        Adress dom_toma = new Adress("Дом Пушкина");
+        Person tom = new Person("Tom", dom_toma);
+        System.out.println(tom.toString());
     }
-
-    interface Shape{
-        void Area();
-    }
-
-    static class Circle implements Shape{
-        double p=3.14;
-        double r;
-        double S;
-        Circle(double r){
-            this.r=r;
-        }
-        public void Area() {
-            S=p*(r*r);
-            System.out.println(S);
-        }
-    }
-    static class Triangle implements Shape{
-        double a;
-        double h;
-        double S;
-        Triangle(double a, double h){
-            this.a=a;
-            this.h=h;
-        }
-        public void Area() {
-            S=(0.5*h*a);
-            System.out.println(S);
-        }
-    }
-    static class Rectangle implements Shape{
-        double a;
-        double b;
-        double S;
-        Rectangle(double a, double b){
-            this.b=b;
-            this.a=a;
-        }
-        public void Area() {
-            S=a*b;
-            System.out.println(S);
+    record Adress(String adress){}
+    record Person(String name, Adress Adress){
+        @Override
+        public String toString() {
+            return String.format("%s живёт в %s", name, Adress.adress);
         }
     }
 }
+
